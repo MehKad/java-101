6c8668f3ec39dc47815e11a57228b30ea863f6e8
MehKad <mehkadiri@gmail.com>
2022-10-13 15:43:13 +0100

    Tableau de multiplication

--- tp-02/Main.java
+++ tp-02/Main.java
@@ -1,24 +1,14 @@
-import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-                                                                        //Tableau avec des valeurs predefinit
-        int[] A = {1,2,3,4};                                            //Declartion du tableau
-        for(int v:A){
-            System.out.println(v);
-        }
-                                                                        //Matrice avec des valeurs entrer par un utilisateur
-        int[][] B = new int [3][3];                                     //Declaration de la matrice
-        Scanner c = new Scanner(System.in);
-        for (int i=0;i<B.length;i++){
-            for(int j=0;j<B.length;j++){
-                B[i][j] = c.nextInt();                                  //Scanf de la matrice
+        int[][] A = new int [11][11];                                     //Declaration de la matrice
+        for (int i=0;i<A.length;i++){
+            for(int j=0;j<A[0].length;j++){
+                A[i][j]=i*j;
+                System.out.print(A[i][j] + "\t");
             }
+            System.out.println();
         }
-        for (int i=0;i<B.length;i++){
-            for(int j=0;j<B.length;j++){
-                System.out.println(B[i][j]);
-            }
-        }
+
         
     }
 }
